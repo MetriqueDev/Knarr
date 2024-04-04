8122f965aaf208c5b3647640aeb6937b97a79787
Tristonks <tristan.bedel@groupe-esigelec.org>
2024-04-04 10:35:48 +0200

    fix destination

--- classes/destination.py
+++ classes/destination.py
@@ -2,31 +2,28 @@ import pygame
 
 class Card_bateau():
 
-	def __init__(self,name,echange=True):
+	def __init__(self,id,cout_coul,gain,gain_col,echange=True):
 		self.echange=echange #if True = ehcnage if False = Influence
 		self.size=(300,150)
 		self.name=name
+		self.id=id
 		self.face="F"
+		self.cout_coul = cout_coul
+		self.gain = gain
+		self.gain_col = gain_col
 		self.init_image()
 
 	def init_image(self):
 		if self.echange:
-			self.image_front_load = pygame.image.load(f".\\images\\echange\\{self.name}.png").convert_alpha() #pour la transaprance on utilise convert_alpha
-			self.image_front = pygame.transform.scale(self.image_front_load, self.size) #changement de taille
-			self.front_rect = self.image_front.get_rect()
-
-			self.image_back_load = pygame.image.load(f".\\images\\echange\\echange_verso.png").convert_alpha() 
-			self.image_back = pygame.transform.scale(self.image_back_load, self.size)
-			self.back_rect = self.image_back.get_rect()
+			self.image_echange_front_load = pygame.image.load(f".\\images\\echange\\echange_+{self.id}.png").convert_alpha() #pour la transaprance on utilise convert_alpha
+			self.image_echange_front = pygame.transform.scale(self.image_echange_front_load, self.size) #changement de taille
+			self.image_echange_back_load = pygame.image.load(f".\\images\\echange\\echange_verso.png").convert_alpha() 
+			self.image_echange_back = pygame.transform.scale(self.image_echange_back_load, self.size)
 		else:
-			self.image_front_load = pygame.image.load(f".\\images\\influence\\{self.name}.png").convert_alpha() #pour la transaprance on utilise convert_alpha
-			self.image_front = pygame.transform.scale(self.image_front_load, self.size) #changement de taille
-			self.front_rect = self.image_front.get_rect()
-
-			self.image_back_load = pygame.image.load(f".\\images\\influence\\influence_verso.png").convert_alpha() 
-			self.image_back = pygame.transform.scale(self.image_back_load, self.size)
-			self.back_rect = self.image_back.get_rect()
-
+			self.image_influence_front_load = pygame.image.load(f".\\images\\influence\\influence_+{self.id}.png").convert_alpha() #pour la transaprance on utilise convert_alpha
+			self.image_influence_front = pygame.transform.scale(self.image_influence_load, self.size) #changement de taille
+			self.image_influence_back_load = pygame.image.load(f".\\images\\influence\\influence_verso.png").convert_alpha() 
+			self.image_influence_back = pygame.transform.scale(self.image_influence_back_load, self.size)
 
 	def print(self,screen):
 		if  self.face =="F":
@@ -37,10 +34,11 @@ class Card_bateau():
 			self.back_rect.x=10
 			self.back_rect.y=150+10
 			screen.blit(self.image_back, (self.back_rect.x,self.back_rect.y))
+	
 
-
-def liste(ech,infl):
-	for i in range(20):
-		ech.append(str("echange_"+str(i+1)))
-	for i in range(15):
-		infl.append(str("influence_"+str(i+1)))
+	def info(self):
+		print(f"échange : {self.echange}")
+		print(f"id : {self.id}")
+		print(f"cout : {self.cout_coul}")
+		print(f"gain : {self.gain}")
+		print(f"gain colonnes : {self.gain_col}")
