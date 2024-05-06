5251fbfbe1b8a356008424f0b778ca3c444058b2
idontknowwwwwww <163175308+idontknowwwwwww@users.noreply.github.com>
2024-05-06 08:36:26 +0200

    Update hand.py

--- classes/hand.py
+++ classes/hand.py
@@ -7,9 +7,9 @@ class Hand:
         self.main=[]
 
     def afficher_main(self, screen, position):
-        x_offset, y_offset = position  # ???position à choisir??Détermine les coordonnées de départ pour afficher les cartes 
+        x_offset, y_offset = (400,200)  # Détermine les coordonnées de départ pour afficher les cartes 
 
         for card in self.main:
           card.print(screen, (x_offset, y_offset))  # Affiche chaque carte à l'emplacement spécifié
-          x_offset += card.size[0] + 10  #?ecard à ajuster? Ajuste la position X pour la prochaine carte, en ajoutant un espacement entre les cartes
+          x_offset += card.size[0] + 10  # Ajuste la position X pour la prochaine carte, en ajoutant un espacement entre les cartes
 
