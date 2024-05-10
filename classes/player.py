1e18688fa6d6724cff647b367f265d9ad678f9e4
idontknowwwwwww <163175308+idontknowwwwwww@users.noreply.github.com>
2024-05-10 15:55:24 +0200

    Update player.py

--- classes/player.py
+++ classes/player.py
@@ -1,5 +1,6 @@
 from classes.boat import Boat
 from classes.hand import Hand
+from classes.package import Package
 
 class Player():
 
@@ -79,7 +80,7 @@ class Player():
                 self.equipage[couleur][i].print(screen,(5+125*x,int(screen.get_height()-taille*len(self.equipage[couleur])+i*taille-self.equipage[couleur][i].size[1])))
             x+=1
     
-    def dragndrop(self,screen,event):
+    def dragndrop_hand(self,screen,event):
         for num, card in enumerate(self.hand):
             print(num)
             if card.front_rect.collidepoint(pygame.mouse.get_pos()):
@@ -101,6 +102,28 @@ class Player():
             if active_card !=None:
                 self.hand[active_card].print(screen,(event.pos[0]-offset_x,event.pos[1]-offset_y))
     
+    def dragndrop_pioche(self,screen,event):
+        for num, card in enumerate(self.pioche):
+            print(num)
+            if card.front_rect.collidepoint(pygame.mouse.get_pos()):
+                active_card=num
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                #position de la souris sur l'image
+                offset_x=mouse_x-self.pioche[active_card].pos[0]
+                offset_y=mouse_y-self.pioche[active_card].pos[1]
+            if event.type == pygame.MOUSEBUTTONUP:            
+                active_card=None
+            elif event.type == pygame.MOUSEMOTION:
+                if active_card != None:
+                    self.pioche[active_card].print(screen,(event.pos[0]-offset_x,event.pos[1]-offset_y))
+                    #print((event.pos[0]-offset_x,event.pos[1]-offset_y))
+                    #print((screen.get_width()/2-100,screen.get_width()/2+100))
+                    #print((screen.get_height()/2+100,screen.get_height()/2-100))
+                
+        #afficher l'image à la souris pendant le drag and drop si on bouge pas
+            if active_card !=None:
+                self.pioche[active_card].print(screen,(event.pos[0]-offset_x,event.pos[1]-offset_y))
+    
     def Ajout_equipage(self,screen,event,equipage):
             if event.button==1:
                 if screen.get_width()/2-100<event.pos[0]<screen.get_width()/2+100 and event.pos[1]>screen.get_height()-200 and active_card != None: #position à adapter à l'equipage
@@ -114,7 +137,14 @@ class Player():
                           self.add_score(1)
                       elif card.gain == "bracelet":
                           self.add_bracelet(1)  
-                  self.hand= self.hand.pop(self.hand[active_card])                             
+                  self.hand= self.hand.pop(self.hand[active_card])
+                  #donner ici le choix entre prendre la carte de meme couleur ou payer 1 recrue pour choisir
+                  #choix a refaire, juste temporaire pr avancer mais à refaire impérativement
+                  choix= int(input(print("1 si vous voulez la meme couleur, 2 si vous payez 1 recrue pour choisir n'importe laquelle:")))
+                  if choix ==1:
+                      self.dragndrop_pioche(screen, event)
+                      
+                      
                   active_card=None
                 else:
                     active_card=None
