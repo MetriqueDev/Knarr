bbc3d4190bd0a24d0c1dd11ebf009987e8d0960f
MetriqueDev <metriquedev@gmail.com>
2024-05-10 20:00:12 +0200

    connexion

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from classes.package_destination import Package_Destination
 import classes.game as game
 from classes.input import Button , Input
 
+import sqlite3
+
+
 #PyGame setup
 pygame.init()
 screen = pygame.display.set_mode((1920,1080),pygame.RESIZABLE) #(1280, 720))
@@ -22,7 +25,6 @@ clock = pygame.time.Clock()
 step="main"
 font = pygame.font.SysFont("arialblack", 40)
 TEXT_COL = (255, 255, 255)
-
 
 #Musique
 #pygame.mixer.music.load(".\\musique\\Dragonborn.mp3")
@@ -44,7 +46,6 @@ fermeture_boutton= Button(screen.get_width()-40,10,fermeture,1)
 btn_unselect_image_load=pygame.image.load(f".\\images\\gui\\btn_unselect.png").convert_alpha() 
 btn_select_image_load=pygame.image.load(f".\\images\\gui\\btn_select.png").convert_alpha() 
 
-
 connexion_boutton= Button(200,200,[btn_unselect_image_load,btn_select_image_load],5,font,"Connexion")
 
 inscription_button= Button(200,350,[btn_unselect_image_load,btn_select_image_load],5,font,"inscription")
@@ -54,26 +55,19 @@ option_boutton= Button(200,500,[btn_unselect_image_load,btn_select_image_load],5
 retour_image_load=pygame.image.load(f".\\images\\gui\\empty.png").convert_alpha() 
 retour_boutton= Button(750,500,retour_image_load,0.2)
 
-
-
 #Initialisation propre
 nbr_player=1
-
-
 
 destination=Package_Destination()
 liste=[]
 
-
 active_card=None
 mouse_pos=[0,0]
-
 
 #focntion de jeu
 def game_process(players):
     for player in players:
         player.add_renome_to_score()
-
 
 while running:
     #screen.fill((0,0,0))
@@ -81,7 +75,7 @@ while running:
     if step == "main":
         screen.blit(main_menu_bg,(0,0))
         if connexion_boutton.draw(screen):
-            step="Connexion"
+            step="connexion"
             connexion_title=font.render("Menu connexion",True,TEXT_COL)
             name_label=font.render("Votre nom:",True,TEXT_COL)
             input_name_image_load=pygame.image.load(f".\\images\\gui\\input.png").convert_alpha() 
@@ -92,11 +86,6 @@ while running:
             input_mdp_boutton= Input(400,450,input_name_image_load,2,font,TEXT_COL)
             valider_boutton= Button(400,700,[btn_unselect_image_load,btn_select_image_load],5,font,"Valider")
             retour_boutton= Button(400+20+5*96,700,[btn_unselect_image_load,btn_select_image_load],5,font,"Retour")
-
-
-            
-
-
 
         if inscription_button.draw(screen):
             step="inscription"
@@ -112,11 +101,11 @@ while running:
             valider_boutton= Button(400,700,[btn_unselect_image_load,btn_select_image_load],5,font,"Valider")
             retour_boutton= Button(400+20+5*96,700,[btn_unselect_image_load,btn_select_image_load],5,font,"Retour")
 
-
         if option_boutton.draw(screen):
             step="option"
             valider_boutton= Button(400,700,[btn_unselect_image_load,btn_select_image_load],5,font,"Valider")
             retour_boutton= Button(400+20+5*96,700,[btn_unselect_image_load,btn_select_image_load],5,font,"Retour")
+
     if step == "play":
         screen.blit(background, (0,0))
         screen.blit(background, (0,0))
@@ -131,24 +120,11 @@ while running:
         if retour_boutton.draw(screen):
             print(step)
             step="main"
+            option_boutton= Button(200,500,[btn_unselect_image_load,btn_select_image_load],5,font,"Option")
 
-    if step == "Connexion":
+    if step=="menu_play":
         screen.blit(main_menu_bg,(0,0))
-
-        screen.blit(connexion_title,(400,100))
-        screen.blit(name_label,(400,200))
-        input_name_boutton.draw(screen)
-
-        screen.blit(mdp_label,(400,400))
-        input_mdp_boutton.draw(screen)
-
-        if valider_boutton.draw(screen):
-            print("Valider")
-            print("name:",input_name_boutton.get_input())
-            print("mdp:",input_mdp_boutton.get_input())
-
-
-
+        if jouer_boutton.draw(screen):
             step="play"
             jeu=game.Game(players)
             jeu.init_game()
@@ -167,10 +143,56 @@ while running:
                 for el in range(3):
                  card= package.pioche_hand(players[i].hand)
 
-        if retour_boutton.draw(screen):
-            print(step)
-            step="main"
+        if option_boutton.draw(screen):
+            step="option"
+            retour_boutton= Button(400+20+5*96,700,[btn_unselect_image_load,btn_select_image_load],5,font,"Retour")
 
+        if retour_boutton.draw(screen):
+            step="main"
+            option_boutton= Button(200,500,[btn_unselect_image_load,btn_select_image_load],5,font,"Option")
+
+
+    if step == "connexion":
+        screen.blit(main_menu_bg,(0,0))
+
+        screen.blit(connexion_title,(400,100))
+        screen.blit(name_label,(400,200))
+        input_name_boutton.draw(screen)
+
+        screen.blit(mdp_label,(400,400))
+        input_mdp_boutton.draw(screen)
+
+        if valider_boutton.draw(screen):
+            #print("Valider")
+            #print("name:",input_name_boutton.get_input())
+            #print("mdp:",input_mdp_boutton.get_input())
+
+
+            name=input_name_boutton.get_input()
+            mdp=input_mdp_boutton.get_input()
+
+            con = sqlite3.connect("data.db")
+            cur = con.cursor()
+            data_connect=cur.execute("SELECT * FROM comptes WHERE nom = '{}'".format(name))
+            data=data_connect.fetchone()
+            cur.close()
+            con.close()
+            if data==None:
+                #print("connais pas")
+                step="main"
+            else:
+                if data[5] == mdp:
+                    step="menu_play"
+                    jouer_boutton= Button(400,100,[btn_unselect_image_load,btn_select_image_load],5,font,"Jouer")
+                    option_boutton= Button(400,250,[btn_unselect_image_load,btn_select_image_load],5,font,"Option")
+                    retour_boutton= Button(400,400,[btn_unselect_image_load,btn_select_image_load],5,font,"Retour")
+                else:
+                    #print("pas le bon mdp")
+                    step="main"
+
+        if retour_boutton.draw(screen):
+            #print(step)
+            step="main"
 
     if step == "inscription":
         screen.blit(main_menu_bg,(0,0))
@@ -183,11 +205,26 @@ while running:
         input_mdp_boutton.draw(screen)
 
         if valider_boutton.draw(screen):
-            print("Valider")
-            print("name:",input_name_boutton.get_input())
-            print("mdp:",input_mdp_boutton.get_input())
+            #print("Valider")
+            #print("name:",input_name_boutton.get_input())
+            #print("mdp:",input_mdp_boutton.get_input())
+
+            name=input_name_boutton.get_input()
+            mdp=input_mdp_boutton.get_input()
+
+            con = sqlite3.connect("data.db")
+            cur = con.cursor()
+            existe=cur.execute("SELECT nom FROM comptes WHERE NOM = '{}'".format(name))
+            if existe==None:
+                cur.execute("INSERT INTO 'comptes' ('nom','victoires','défaites','niveau','mdp') VALUES (?,?,?,?,?)",(name,0,0,0,mdp))
+                con.commit()
+            cur.close()
+            con.close()
+
+
+            step="main"#il est inscrit mais il doit se connecter donc retour au premier écran
         if retour_boutton.draw(screen):
-            print(step)
+            #print(step)
             step="main"
 
     if step == "option":
@@ -196,15 +233,15 @@ while running:
         screen.blit(img,(400,500))
         valider_boutton.draw(screen)
         if retour_boutton.draw(screen):
-            print(step)
+            #print(step)
             step="main"
-
+            option_boutton= Button(200,500,[btn_unselect_image_load,btn_select_image_load],5,font,"Option")
 
     if fermeture_boutton.draw(screen):
         break
 
     for event in pygame.event.get():
-        if step == "inscription":
+        if step == "inscription" or step=="connexion":
             input_name_boutton.write(event)
             input_mdp_boutton.write(event)
 
