fc857e8af4c1c70c84a7a3270c89432a2f02b576
Tristonks <tristan.bedel@groupe-esigelec.org>
2024-05-17 11:43:56 +0200

    server test

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('localhost', 12345))
+
+s.send(b'Hello, Server!')
+message = s.recv(1024)
+print('Received from server: ', message)
+
+# Output:
+# Received from server:  b'Hello, Client!'
---
+++ server.py
@@ -0,0 +1,22 @@
+import socket
+import threading
+
+def handle_client(c):
+    message = c.recv(1024)
+    print('Received:', message)
+    c.send(b'Echo:' + message)
+    c.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost', 12345))
+s.listen(5)
+
+while True:
+    c, addr = s.accept()
+    print('Got connection from', addr)
+    threading.Thread(target=handle_client, args=(c,)).start()
+
+# Output:
+# Got connection from ('127.0.0.1', 52617)
+# Received: b'Hello, Server!'
+# ...
