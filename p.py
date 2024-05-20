248c89b7099fdcf163e9f5fed060584878816134
MetriqueDev <metriquedev@gmail.com>
2024-05-20 18:41:35 +0200

    La tuile

--- p.py
+++
@@ -1,9 +0,0 @@
-from classes.destination import Destination
-
-
-with open("cartes_destination.txt","w") as file:
-    for i in range(15):
-        a=i+1
-        b=(Destination(str(input("influ ou ech ?")), str(input("cout en couleur")), int(input('cout en recrues'))))
-        b.image(a)
-        file.write(b)
